fa8b6650ac286b52803b98b6b596f3cdc9db87cb
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2020-03-02 00:48:52 -0500

    initial initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+pynoob3a
+========
+
+Python implementation of dumb arrays (see [noob3a](https://github.com/noobermin/noob3a)).
+
+Documentation
+-------------
+
+later
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pynoob3a',
+      version='0.0.1',
+      description='dumb binary format',
+      author='noobermin',
+      author_email='ngirmang@protonmail.com',
+      license='MIT',
+      packages=['noob3a'],
+      zip_safe=False);
